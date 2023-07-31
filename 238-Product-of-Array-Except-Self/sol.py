08ec6842b6f03ecdbaeed44801bfb59da24118ff
oyoungsun <oyoungsun1030@gmail.com>
2023-08-01 01:09:02 +0900

    Runtime 509ms(99.34) Memory 26.16mb(87.69%)

--- 238-Product-of-Array-Except-Self/sol.py
+++ 238-Product-of-Array-Except-Self/sol.py
@@ -1,23 +1,13 @@
 class Solution(object):
-    def containsDuplicate(self, nums):
+    def maxSubArray(self, nums):
         """
         :type nums: List[int]
-        :rtype: bool
+        :rtype: int
         """
-        # nums = sorted(nums)
-        # prev = nums[0]
-        # for i in range(1, len(nums)):
-        #     if(prev==nums[i]) : return True
-        #     else : prev = nums[i]
-        # return False
-        map = {}
-        for i in range(0, len(nums)):
-            if nums[i] in map : return True
-            else : map[nums[i]] = i
-        return False
-        # if len(nums)==len(set(nums)):  이렇게 set()으로
-        #  길이를 비교하면
-        # 더 높은 성능으로 측정됨.
-        #     return False 
-        #     else:
-        #     return True
+        total=0
+        maxS=nums[0]
+        for n in nums:
+            if total<0: total=0
+            total+=n
+            maxS = max(total, maxS)
+        return maxS
